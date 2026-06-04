7a1a38415344e0e6a1116ccfc54093f32d77df6c
polymorvic <polymorphic.bite@gmail.com>
2026-06-04 16:43:03 +0200

    feat(geometry): initialize geometry module with imports for points, lines, segments, and linestrings

---
+++ src/cvgeomkit/geometry/__init__.py
@@ -0,0 +1,4 @@
+from cvgeomkit.geometry.points import Point, transform_point
+from cvgeomkit.geometry.lines import Line, LineGroup, transform_line, group_lines
+from cvgeomkit.geometry.segments import LineSegment, transform_line_segment
+from cvgeomkit.geometry.linestrings import LineString, transform_linestring
